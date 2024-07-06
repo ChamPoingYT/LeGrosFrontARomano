4677f9e956dc716f7c9dc91ddaf1fa8d197c17d6
ChamPoingYT <champoing.pro@gmail.com>
2024-07-06 17:02:22 +0200

    ajout de l'interface pour s'enregistrer

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+// Utilisateurs fictifs stockés en mémoire pour l'exemple
+let users = [
+    { username: 'admin', password: 'admin123' }
+];
+
+// Gestion de la soumission du formulaire de connexion
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     
-    // Logique de validation simplifiée pour l'exemple
-    if (username === 'admin' && password === 'admin123') {
+    let user = users.find(user => user.username === username && user.password === password);
+    
+    if (user) {
         alert('Connexion réussie');
         // Rediriger ou effectuer une action après une connexion réussie
     } else {
         document.getElementById('error-message').innerText = 'Nom d\'utilisateur ou mot de passe incorrect';
     }
 });
+
+// Gestion de la soumission du formulaire d'inscription
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var newUsername = document.getElementById('newUsername').value;
+    var newPassword = document.getElementById('newPassword').value;
+    
+    let userExists = users.find(user => user.username === newUsername);
+    
+    if (userExists) {
+        document.getElementById('signup-error-message').innerText = 'Nom d\'utilisateur déjà pris';
+    } else {
+        users.push({ username: newUsername, password: newPassword });
+        alert('Inscription réussie');
+        window.location.href = 'index.html'; // Rediriger vers la page de connexion après inscription
+    }
+});
+
